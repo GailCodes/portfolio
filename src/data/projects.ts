e848712239d6edaa6266687160d12f40f64ad307
GailCodes <gail_panda@proton.me>
2025-08-20 16:49:33 -0700

    Add projects as data and bold text

---
+++ src/data/projects.ts
@@ -0,0 +1,46 @@
+import type { ProjectData } from "../types/ProjectData";
+
+export const projects: ProjectData[] = [
+  {
+    title: "Google",
+    description:
+      "The world's most popular search engine. Made with HTML, Javascript and PHP",
+    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
+    url: "https://www.google.com",
+  },
+  {
+    title: "Reverse Snake",
+    description:
+      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript",
+    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
+    url: "https://www.google.com",
+  },
+  {
+    title: "Reverse Snake",
+    description:
+      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript",
+    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
+    url: "https://www.google.com",
+  },
+  {
+    title: "Reverse Snake",
+    description:
+      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript",
+    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
+    url: "https://www.google.com",
+  },
+  {
+    title: "Reverse Snake",
+    description:
+      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript",
+    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
+    url: "https://www.google.com",
+  },
+  {
+    title: "Reverse Snake",
+    description:
+      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript",
+    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
+    url: "https://www.google.com",
+  },
+];
