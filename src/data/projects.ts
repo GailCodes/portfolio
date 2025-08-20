1e0470819066b2d408828533877003db9e482d87
GailCodes <gail_panda@proton.me>
2025-08-20 16:54:12 -0700

    Change card text alignment

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,42 +4,42 @@ export const projects: ProjectData[] = [
   {
     title: "Google",
     description:
-      "The world's most popular search engine. Made with HTML, Javascript and PHP",
+      "The world's most popular search engine. Made with HTML, Javascript and PHP.",
     imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
     url: "https://www.google.com",
   },
   {
     title: "Reverse Snake",
     description:
-      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript",
+      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript.",
     imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
     url: "https://www.google.com",
   },
   {
     title: "Reverse Snake",
     description:
-      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript",
+      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript.",
     imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
     url: "https://www.google.com",
   },
   {
     title: "Reverse Snake",
     description:
-      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript",
+      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript.",
     imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
     url: "https://www.google.com",
   },
   {
     title: "Reverse Snake",
     description:
-      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript",
+      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript.",
     imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
     url: "https://www.google.com",
   },
   {
     title: "Reverse Snake",
     description:
-      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript",
+      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript.",
     imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
     url: "https://www.google.com",
   },
