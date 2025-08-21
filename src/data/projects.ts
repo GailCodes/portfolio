fe2b394caf7edc5953f6748276e373e290b5cf88
GailCodes <gail_panda@proton.me>
2025-08-21 14:52:40 -0700

    change images to public folder

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,35 +4,35 @@ export const projects: ProjectData[] = [
   {
     title: "Hyperstack",
     description: "Cloud Computing Platform. Made with Next.js & Tailwind CSS.",
-    imageUrl: "/src/assets/images/project-images/hyperstack.jpg",
+    imageUrl: "/images/project-images/hyperstack.jpg",
     url: "https://www.hyperstack.cloud/",
   },
   {
     title: "Next.js Blog",
     description:
       "A high-performance, statically-generated blog built with Next.js.",
-    imageUrl: "/src/assets/images/project-images/next-blog.png",
+    imageUrl: "/images/project-images/next-blog.png",
     url: "https://react-blog-amber-beta.vercel.app/",
   },
   {
     title: "Reverse Snake",
     description:
       "A twist on the classic Snake game built with Javascript and HTML Canvas.",
-    imageUrl: "/src/assets/images/project-images/reverse-snake.png",
+    imageUrl: "/images/project-images/reverse-snake.png",
     url: "https://gailcodes.github.io/reverse-snake/",
   },
   {
     title: "Coffee Machine",
     description:
       "A fun and interactive web-based coffee machine built entirely with HTML, CSS, and JavaScript.",
-    imageUrl: "/src/assets/images/project-images/coffee-machine.png",
+    imageUrl: "/images/project-images/coffee-machine.png",
     url: "https://gailcodes.github.io/coffee-machine/",
   },
   {
     title: "Simple Calculator",
     description:
       "A sleek, and simple calculator web application built with HTML, CSS, and Javascript",
-    imageUrl: "/src/assets/images/project-images/simple-calculator.png",
+    imageUrl: "/images/project-images/simple-calculator.png",
     url: "https://gailcodes.github.io/calculator-js/",
   },
 ];
