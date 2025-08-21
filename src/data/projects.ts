80b12a7a85c41c879a2bb9c060f78bdc95d0ce14
GailCodes <gail_panda@proton.me>
2025-08-21 14:39:10 -0700

    add real projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,45 +2,37 @@ import type { ProjectData } from "../types/ProjectData";
 
 export const projects: ProjectData[] = [
   {
-    title: "Google",
+    title: "Hyperstack",
+    description: "Cloud Computing Platform. Made with Next.js & Tailwind CSS.",
+    imageUrl: "/src/assets/images/project-images/hyperstack.jpg",
+    url: "https://www.hyperstack.cloud/",
+  },
+  {
+    title: "Next.js Blog",
     description:
-      "The world's most popular search engine. Made with HTML, Javascript and PHP.",
-    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
-    url: "https://www.google.com",
+      "A high-performance, statically-generated blog built with Next.js.",
+    imageUrl: "/src/assets/images/project-images/next-blog.png",
+    url: "https://react-blog-amber-beta.vercel.app/",
   },
   {
     title: "Reverse Snake",
     description:
-      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript.",
-    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
-    url: "https://www.google.com",
+      "A twist on the classic Snake game built with Javascript and HTML Canvas.",
+    imageUrl: "/src/assets/images/project-images/reverse-snake.png",
+    url: "https://gailcodes.github.io/reverse-snake/",
   },
   {
-    title: "Reverse Snake",
+    title: "Coffee Machine",
     description:
-      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript.",
-    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
-    url: "https://www.google.com",
+      "A fun and interactive web-based coffee machine built entirely with HTML, CSS, and JavaScript.",
+    imageUrl: "/src/assets/images/project-images/coffee-machine.png",
+    url: "https://gailcodes.github.io/coffee-machine/",
   },
   {
-    title: "Reverse Snake",
+    title: "Simple Calculator",
     description:
-      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript.",
-    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
-    url: "https://www.google.com",
-  },
-  {
-    title: "Reverse Snake",
-    description:
-      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript.",
-    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
-    url: "https://www.google.com",
-  },
-  {
-    title: "Reverse Snake",
-    description:
-      "Snake game with a twist! YOU are the food. Made with HTML Canvas and Javascript.",
-    imageUrl: "/src/assets/images/project-images/dummy-card-image.jpg",
-    url: "https://www.google.com",
+      "A sleek, and simple calculator web application built with HTML, CSS, and Javascript",
+    imageUrl: "/src/assets/images/project-images/simple-calculator.png",
+    url: "https://gailcodes.github.io/calculator-js/",
   },
 ];
